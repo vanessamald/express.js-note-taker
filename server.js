f355e7c7eac178193900a998eb0d973bdc6a032d
Vanessa Maldonado <vanessa.rodriguez3@mavs.uta.edu>
2022-06-05 19:52:12 -0500

    setting up port and connecting html/css

---
+++ server.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+
+// require express
+const express = require('express');
+
+// initiate the server
+const app = express();
+
+// add index.js
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+});
+
+// middleware for css
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+//const { notes } = require('./Develop/public/assets');
+
+//app.get('api/notes', (req, res) => {
+   // res.send('Hello World!');
+//});
+
+
+
+// make our server listen
+app.listen(3001, () => {
+    console.log('PROJECT NOW ON PORT 3001!')
+});
