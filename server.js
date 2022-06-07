c022d78df63a493f4d1abf739b2460ab2b11806e
Vanessa Maldonado <vanessa.rodriguez3@mavs.uta.edu>
2022-06-07 14:56:53 -0500

    adding api post route

--- server.js
+++ server.js
@@ -1,22 +1,18 @@
 const fs = require('fs');
 const path = require('path');
 
+
 //const apiRoutes = require('./Develop/public/assets/js');
 
 // api routes
-//const { notes } = require('./Develop/db/db');
+const notes = require('./Develop/db/db.json');
 
 // require express
 const express = require('express');
+const { readList } = require('./Develop/db/db');
 
 // initiate the server
 const app = express();
-
-// add index.js
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
-});
-
 
 //app.use(express.static(path.join(__dirname, './Develop/public/assets/js/index.js')));
 
@@ -28,36 +24,72 @@ app.use(express.json());
 // middleware for css
 app.use(express.static('public'));
 
-
-//const { notes } = require('./Develop/public/notes.html');
+// add index.js
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+});
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
     //res.send('Hello World!');
 });
 
-/* TESTING API ROUTES IN API-ROUTES.JS
-// api route
+// GET api route
 app.get('/api/notes', (req, res) => {
-    res.json(notes);
-});
-
-// POST route
-app.post('/api/notes', (req, res) => {
-    console.log(req.body);
     res.json(req.body);
+    console.log(req.body);
+
+
+
+
 });
-*/
 
+// POST api route ROUTE TESTED AND WORKING
+app.post('/api/notes', (req, res) => {
+    //TESTING HERE
+    console.info(`${req.method} request received to add a note`);
+    
+    // destructure req.body
+    const [ {title, text} ] = req.body
 
+    // if properties exist
+    if (title && text) {
 
-//app.use('/notes', apiRoutes);
+    // variable for notes 
+    const newNote = {
+        title,
+        text
+    };
 
-// TESTING API ROUTES CONNECTION
-// api routes
-const apiRoutes = require('./routes/api-routes');
-app.use(apiRoutes);
+    // obtain existing notes
+    fs.readFile('./Develop/db/db.json', 'utf8', (err, data) => {
+    
+    res.json(req.body);
+    //console.log(req.body);
+        if (err) {
+            console.error(err);
+        } else {
+            // convert string to JSON object
+            const parsedNotes = JSON.parse(data);
+            // parse data
+            parsedNotes.push(newNote);
 
+            
+            fs.writeFileSync('./Develop/db/db.json', JSON.stringify(parsedNotes, null, 4),
+            (writeErr) =>
+            writeErr
+            ? console.error(writeErr)
+            : console.info('SUCCESS!!!')
+           );
+        }
+      });
+  
+    
+
+    
+    }
+});
+            
 // wildcard route
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './Develop/public/index.html'));
